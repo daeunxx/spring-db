9418764f7ecb8fc9c2705f4c5f2eb1f423e7469b
daeun <sss3598@gmail.com>
2024-07-21 19:06:40 +0900

    JdbcTemplate | 테스트 임베디드(메모리) 모드 설정

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,11 +2,13 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 
 //@Import(MemoryConfig.class)
@@ -26,4 +28,16 @@ public class ItemServiceApplication {
     return new TestDataInit(itemRepository);
   }
 
+  /*
+  @Bean
+  @Profile("test")
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); //임베디드(메모리) 모드에서 H2 DB 동작
+    dataSource.setUsername("sa");
+    dataSource.setPassword("");
+    return dataSource;
+  }
+  */
 }
